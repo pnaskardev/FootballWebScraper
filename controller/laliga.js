95679515b676d475b1d28a55b08cbe3c3c5047e8
pnaskardev <priyanshunaskar89@gmail.com>
2023-04-06 19:48:10 +0530

    we have come close to the final element but one of the component html is not getting selected

--- controller/laliga.js
+++ controller/laliga.js
@@ -1,6 +1,31 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
 
+
+const url = 'https://www.flashscore.com/football/spain/laliga/standings/#/COQ6iu30/table/overall';
 
 exports.getlaliga=(req,res,next)=>
 {
-    console.log("inside getLaliga block");
+   
+    axios.get(url).then(response=>
+    {
+        var list=[];
+        console.log("inside getLaliga block");
+        const $=cheerio.load(response.data);
+        const fullPage=$('.container');
+        // console.log(fullPage.html());
+        // const allDivElements = fullPage.find('div');
+        // const tableDiv = fullPage.find('.tournament-table-standings');
+        const tableDiv = fullPage.find('#tournament-table');
+        console.log(tableDiv.attr('id'));
+        console.log(tableDiv.html());
+        // const allDivInsideTable=tableDiv.children();
+        // console.log(allDivInsideTable.length);
+        // allDivInsideTable.each(function(i, element) 
+        // {   
+        //     console.log('1');
+        //     console.log($(this).attr('class'));
+        // });
+    });
 }
