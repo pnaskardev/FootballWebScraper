03d94d42959ec53769d021d2e9146033476dbf7c
pnaskardev <priyanshunaskar89@gmail.com>
2023-04-06 22:46:45 +0530

    we are accessing the table directly and we can start scraping data with some for loops

--- controller/laliga.js
+++ controller/laliga.js
@@ -3,23 +3,25 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 
 
-const url = 'https://www.flashscore.com/football/spain/laliga/standings/#/COQ6iu30/table/overall';
+const url = 'https://www.espn.in/football/table/_/league/esp.1';
 
 exports.getlaliga=(req,res,next)=>
 {
-   
+    var list=[];
+    console.log("inside getLaliga block");
     axios.get(url).then(response=>
     {
-        var list=[];
-        console.log("inside getLaliga block");
-        const $=cheerio.load(response.data);
-        const fullPage=$('.container');
-        // console.log(fullPage.html());
+        const $=cheerio.load
+        (
+            response.data,
+        );
+        const fullPage=$('.Table__TBODY');
+        console.log(fullPage.attr('class'));
         // const allDivElements = fullPage.find('div');
         // const tableDiv = fullPage.find('.tournament-table-standings');
-        const tableDiv = fullPage.find('#tournament-table');
-        console.log(tableDiv.attr('id'));
-        console.log(tableDiv.html());
+        // const tableDiv = fullPage.find('#tournament-table');
+        // console.log(tableDiv.attr('id'));
+        // console.log(tableDiv.html());
         // const allDivInsideTable=tableDiv.children();
         // console.log(allDivInsideTable.length);
         // allDivInsideTable.each(function(i, element) 
